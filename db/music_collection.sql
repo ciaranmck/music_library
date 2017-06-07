4d4e176f3d15e2924cd12df7924eb442597154f3
Ciaran McKenna <ciarantsc@gmail.com>
2017-06-07 14:30:58 +0100

    1st commit

---
+++ db/music_collection.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE artists (
+  id SERIAL2 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE albums;
+  id SERIAL2 PRIMARY KEY,
+  album_title VARCHAR(255),
+  genre VARCHAR(255),
+  artist_id INT2 REFERENCES artists(id)
+  );
